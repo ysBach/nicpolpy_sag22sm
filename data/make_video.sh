b0bcf6ece49a3ec7edcd8a360cd9714a37e0bf38
ysBach <ysbach93@gmail.com>
2022-09-24 15:40:20 +0900

    add media and data

---
+++ data/make_video.sh
@@ -0,0 +1,6 @@
+convert -delay 50 -quality 100% -loop 7 figs/j_INSROT*.png j_INSROT_raw.mp4
+convert -delay 50 -quality 100% -loop 7 figs/h_INSROT*.png h_INSROT_raw.mp4
+convert -delay 50 -quality 100% -loop 7 figs/k_INSROT*.png k_INSROT_raw.mp4
+convert -delay 50 -quality 100% -loop 7 figs/j_POLAGL*.png j_POLAGL_raw.mp4
+convert -delay 50 -quality 100% -loop 7 figs/h_POLAGL*.png h_POLAGL_raw.mp4
+convert -delay 50 -quality 100% -loop 7 figs/k_POLAGL*.png k_POLAGL_raw.mp4
